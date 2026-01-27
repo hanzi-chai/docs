7e53cc50da96c6345d8f0052f2b1cbd69fa77312
Songchen Tan <i@tansongchen.com>
2026-01-27 18:46:42 -0500

    添加文档

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,10 @@ export default defineConfig({
 		starlight({
 			title: '汉字自动拆分系统文档',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			tableOfContents: {
+				minHeadingLevel: 1,
+				maxHeadingLevel: 3,
+			},
       plugins: [
         // Generate the documentation.
         starlightTypeDoc({
@@ -23,15 +27,20 @@ export default defineConfig({
 						{ label: '快速开始', slug: 'tutorial' },
 						{ label: '设计方案的元素', slug: 'tutorial/element' },
 						{ label: '配置拆分方式', slug: 'tutorial/analysis' },
+						{ label: '生成元素序列表和码表', slug: 'tutorial/encode' },
 						{ label: '自定义字形数据', slug: 'tutorial/data' },
+						{ label: '优化方案', slug: 'tutorial/optimization' },
+						{ label: '查看统计数据', slug: 'tutorial/statistics' },
+						{ label: '和现有码表校对', slug: 'tutorial/debug' },
+						{ label: '生成字根图', slug: 'tutorial/diagram' },
 					],
 				},
 				{
 					label: '参考',
 					items: [
-						{ label: "算法参考", slug: 'reference/algorithm' },
 						{ label: '配置项', slug: 'reference/config' },
-						{ label: 'API 参考', slug: 'reference/api' },
+						{ label: "拆分算法", slug: 'reference/algorithm' },
+						{ label: 'API', slug: 'reference/api' },
 						{ label: '数据', slug: 'reference/data' },
 					],
 				},
